f0df3cdbcfb3eb9a7b0bf8bddcf4c663928d4fb8
Gitit2meBaby <thisworldofdans@gmail.com>
2023-09-24 17:09:34 +0700

    Drag and drop finally done, used attributes instead of Y position, closeBtn on hover works and removes todo item, need to work on updating to do count, todo filter and clear completed. and reponsive design..

--- script.js
+++ script.js
@@ -1,51 +1,67 @@
 const input = document.querySelector('#createTodo');
 const inputCheckbox = document.querySelector('#inputCheckbox');
-const todoContainer = document.querySelector('#todoContainer')
+const todoContainer = document.querySelector('#todoContainer');
 let listItem;
-
 
 // CREATING A TODO
 inputCheckbox.addEventListener('click', () => {
-    listItem = input.value
-    console.log(listItem)
-    createTodo()
-    input.value = ''
-})
+    listItem = input.value;
+    createTodo();
+    input.value = '';
+});
+
+input.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        listItem = input.value;
+        createTodo();
+        input.value = '';
+    }
+});
 
 function createTodo() {
-    const newTodo = document.createElement('div')
-    newTodo.classList.add('todo')
+    const newTodo = document.createElement('div');
+    newTodo.classList.add('todo');
+    newTodo.draggable = true;
     newTodo.innerHTML = `
     <div>
-            <div class="checkbox list-checkbox">
-              <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9">
+    <div class="gradient-border">
+        <div class="checkbox list-checkbox">
+            <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9">
                 <path fill="none" stroke="#FFF" stroke-width="2" d="M1 4.304L3.696 7l6-6" />
-              </svg>
-            </div>
-            <p>${listItem}</p>
-          </div>
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18">
-            <path fill="#494C6B" fill-rule="evenodd"
-              d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z" />
-          </svg>
+            </svg>
+        </div>
+        </div>
+        <p>${listItem}</p>
+    </div>
+    <svg class="close-btn hidden" xmlns="http://www.w3.org/2000/svg" width="18" height="18">
+        <path fill="#494C6B" fill-rule="evenodd"
+            d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z" />
+    </svg>
     `;
-    todoContainer.appendChild(newTodo)
+
+
+    // CLOSE BUTTON ACTIONS
+    const closeBtn = newTodo.querySelector('.close-btn');
+    // Show close button on hover
+    newTodo.addEventListener('mouseenter', () => {
+        closeBtn.classList.remove('hidden');
+    });
+
+    // Hide close button when not hovering
+    newTodo.addEventListener('mouseleave', () => {
+        // const closeBtn = newTodo.querySelector('.close-btn');
+        closeBtn.classList.add('hidden');
+    });
+
+    closeBtn.addEventListener('click', () => {
+        newTodo.remove()
+    })
+
+
+
+    todoContainer.insertBefore(newTodo, todoContainer.firstChild);
+    setupDragAndDrop();
 }
-
-// //MARKING TODO AS DONE
-// const listItemCheckbox = document.querySelectorAll('.list-checkbox')
-
-// listItemCheckbox.forEach((check) => {
-//     check.addEventListener('click', (event) => {
-//         console.log('clicked');
-//         const listItem = event.target.closest('todo')
-//         listItem.classList.add('checked')
-//         check.style.background = 'var(--Check-Background)'
-//         checkedText = check.closest('p')
-//         checkedText.style.color = 'var(--placeholder-text)'
-//         checkedText.style.textDecoration = 'strikethrough'
-//     })
-// })
 
 //MARKING TODO AS DONE
 todoContainer.addEventListener('click', (event) => {
@@ -54,8 +70,11 @@ todoContainer.addEventListener('click', (event) => {
     // Check if the clicked element has the class 'list-checkbox'
     if (clickedElement.classList.contains('list-checkbox')) {
         const listItem = clickedElement.closest('.todo');
+        const gradientBorder = clickedElement.closest('.gradient-border');
+        gradientBorder.style.backgroundColor = "white"
         listItem.classList.add('checked');
         clickedElement.style.background = 'var(--Check-Background)';
+        clickedElement.style.border = 'none'
 
         const checkedText = listItem.querySelector('p');
         checkedText.style.color = 'var(--Light-Grayish-Blue)';
@@ -63,3 +82,49 @@ todoContainer.addEventListener('click', (event) => {
         checkedText.style.fontWeight = '400'
     }
 });
+
+
+// DRAGGING TODO'S
+function setupDragAndDrop() {
+    const todos = document.querySelectorAll('.todo');
+    const container = document.querySelector('#todoContainer');
+
+    todos.forEach((todo, index) => {
+        todo.draggable = true;
+        todo.dataset.position = index;
+
+        todo.addEventListener('dragstart', (e) => {
+            e.dataTransfer.setData('text/plain', todo.dataset.position);
+        });
+
+        todo.addEventListener('dragover', (e) => {
+            e.preventDefault();
+        });
+
+        todo.addEventListener('drop', (e) => {
+            e.preventDefault();
+            const position = e.dataTransfer.getData('text/plain');
+            const draggedTodo = document.querySelector(`[data-position="${position}"]`);
+            const dropTarget = todo;
+
+            if (draggedTodo && dropTarget) {
+                const newPosition = dropTarget.dataset.position;
+                if (newPosition !== position) {
+                    // Update the positions
+                    const containerChildren = Array.from(container.children);
+                    containerChildren.splice(containerChildren.indexOf(draggedTodo), 1);
+                    containerChildren.splice(newPosition, 0, draggedTodo);
+
+                    // Rearrange the todos based on the updated positions
+                    container.innerHTML = '';
+                    containerChildren.forEach((child, index) => {
+                        child.dataset.position = index;
+                        container.appendChild(child);
+                    });
+                }
+            }
+        });
+    });
+}
+
+setupDragAndDrop();
